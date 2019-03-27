d21c851f8ea266a1adb37cba4be066e8bac7e44a
Chase Pierce <syntaqx@gmail.com>
2019-03-27 03:46:43 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# git-hooks
+
+[pre-commit]: http://pre-commit.com/
+
+A collection of useful Git hooks for integration with [pre-commit][].
+
+## Usage
+
+Create or append to your `.pre-commit-config.yml` configuration:
+
+```yaml
+- repo: https://github.com/syntaqx/git-hooks
+  hooks:
+    - id: circleci-config-validate
+```
+
+Then simply install or run the checks on-demand as desired.
+
+## License
+
+[MIT]: https://opensource.org/licenses/MIT
+
+`git-hooks` is open source software released under the [MIT license][MIT].
---
+++ hooks/circleci-config-validate.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Test if the CircleCI config file is well formed via the circleci local cli.
+# https://circleci.com/docs/2.0/local-cli/
+# https://circleci.com/blog/circleci-hacks-validate-circleci-config-on-every-commit-with-a-git-hook/
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+readonly DEBUG=${DEBUG:-unset}
+[[ $DEBUG != unset ]] && set -x
+
+# assert the circleci command exists
+if ! [ -x "$(command -v circleci)" ]; then
+    echo 'circleci command not found'
+    echo 'See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
+    exit 1
+fi
+
+# The following is needed by the circleci local build tool (due to Docker interactivity)
+exec < /dev/tty
+
+# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+if ! msg=$(circleci config validate); then
+	echo "CircleCI Configuration Failed Validation."
+	echo "$msg"
+	exit 1
+fi
