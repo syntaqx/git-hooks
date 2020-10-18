92829fe7c539d7533d6da69e6ad1d80228da09e3
Andrew Griffiths <mail@andrewgriffithsonline.com>
2020-10-18 18:08:05 +0100

    circle validate hook does not run in CI

--- hooks/circleci-config-validate.sh
+++ hooks/circleci-config-validate.sh
@@ -5,23 +5,21 @@ readonly DEBUG=${DEBUG:-unset}
 [[ $DEBUG != unset ]] && set -x
 
 # do not run in Circle CI
-if [[ -n $CIRCLECI ]]; then
-	exit 0
-fi
+if [[ -z $CIRCLECI ]]; then
+	# assert the circleci command exists
+	if ! [ -x "$(command -v circleci)" ]; then
+		echo 'circleci command not found'
+		echo 'See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
+		exit 1
+	fi
 
-# assert the circleci command exists
-if ! [ -x "$(command -v circleci)" ]; then
-    echo 'circleci command not found'
-    echo 'See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
-    exit 1
-fi
+	# The following is needed by the circleci local build tool (due to Docker interactivity)
+	[ -f /dev/tty ] && exec /dev/tty
 
-# The following is needed by the circleci local build tool (due to Docker interactivity)
-[ -f /dev/tty ] && exec /dev/tty
-
-# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
-if ! msg=$(circleci config validate); then
-	echo "CircleCI Configuration Failed Validation."
-	echo "$msg"
-	exit 1
+	# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+	if ! msg=$(circleci config validate); then
+		echo "CircleCI Configuration Failed Validation."
+		echo "$msg"
+		exit 1
+	fi
 fi
