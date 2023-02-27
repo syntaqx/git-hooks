911463bc70a1940fd5f79a65e5ebd1a5c2da3d60
Derrick Bryant <90715268+dbryant4-cj@users.noreply.github.com>
2023-02-27 10:34:02 -0500

    Add CircleCI orb validation

---
+++ hooks/circleci-orb-validate.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+readonly DEBUG=${DEBUG:-unset}
+[[ $DEBUG != unset ]] && set -x
+
+# do not run in Circle CI
+if [[ -z $CIRCLECI ]]; then
+
+	#assert repo is a CircleCI Orb
+	if ! [ ! -f "src/@orb.yml" ]; then
+		echo 'Not a CircleCI orb repo. Skipping'
+		exit 0
+	fi
+
+	# assert the circleci command exists
+	if ! [ -x "$(command -v circleci)" ]; then
+		echo 'circleci command not found'
+		echo 'See https://circleci.com/docs/2.0/local-cli/ for installation instructions.'
+		exit 1
+	fi
+
+	# The following is needed by the circleci local build tool (due to Docker interactivity)
+	[ -f /dev/tty ] && exec /dev/tty
+
+	# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+	if ! msg=$(circleci orb pack src | circleci orb validate -); then
+		echo "CircleCI Orb Failed Validation."
+		echo "$msg"
+		exit 1
+	fi
+fi
